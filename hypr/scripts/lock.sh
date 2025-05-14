5411e41d89f294f92e4ce813cd88c4cc970ecd99
KinsaEng <tunaeng@proton.me>
2025-05-14 19:43:29 +0300

    2025-05-14

--- hypr/scripts/lock.sh
+++ hypr/scripts/lock.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-pkill -f showdesk.py
-
-python3 ~/.config/hypr/scripts/showdesk.py
+python3 ~/.config/hypr/scripts/showdesk.py &
 
 swaync-client -dn &
 
 ~/.config/hypr/scripts/minimizeall.sh --minimize &
 
-hyprlock
+hyprlock 
 
 ~/.config/hypr/scripts/minimizeall.sh --return &
 
-swaync-client -df 
+pkill -f showdesk.py &
+
+swaync-client -df &
